e24fe0997e4a2d7459d944ef29f48d9fe5a578ad
namantou <namantou@gmail.com>
2014-10-20 22:50:40 +0800

    报告时间间隔调整为3天

--- src/android/romstats/ReportingServiceManager.java
+++ src/android/romstats/ReportingServiceManager.java
@@ -29,7 +29,7 @@ import android.util.Log;
 public class ReportingServiceManager extends BroadcastReceiver {
 
 	private static final long MILLIS_PER_HOUR = 60L * 60L * 1000L;
-	private static final long MILLIS_PER_DAY = 24L * MILLIS_PER_HOUR;
+	private static final long MILLIS_PER_DAY = 72L * MILLIS_PER_HOUR;
 
 	// UPDATE_INTERVAL days is set in the build.prop file
 	// private static final long UPDATE_INTERVAL = 1L * MILLIS_PER_DAY;
